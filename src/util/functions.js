6c983d7e73342b6d55300f3af4d04a963de7b664
Abner Rodrigues <abnerodrigs@gmail.com>
2021-04-26 18:52:33 -0300

    fix: added @c.us in Group Functions

--- src/util/functions.js
+++ src/util/functions.js
@@ -3,13 +3,13 @@ export function contactToArray(number) {
     if (Array.isArray(number)) {
         for (const contact of number) {
             if (contact !== "")
-                localArr.push(contact);
+                localArr.push(`${contact}@c.us`);
         }
     } else {
         let arrContacts = number.split(/\s*[,;]\s*/g);
         for (const contact of arrContacts) {
             if (contact !== "")
-                localArr.push(contact);
+                localArr.push(`${contact}@c.us`);
         }
     }
 
