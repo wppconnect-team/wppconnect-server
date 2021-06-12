40884becc2cee952a9673f39c539130015d69f50
rodriguesabner <abnerodrigs@gmail.com>
2021-06-12 17:42:59 -0300

    FIX: error isConnected is not a function

--- src/middleware/statusConnection.js
+++ src/middleware/statusConnection.js
@@ -15,7 +15,15 @@
  */
 export default async function statusConnection(req, res, next) {
     try {
-        await req.client.isConnected();
+        if (req.client) {
+            await req.client.isConnected();
+        } else {
+            return res.status(400).json({
+                response: false,
+                status: "Disconnected",
+                message: 'A sessão do WhatsApp não está ativa.'
+            })
+        }
         next();
     } catch (error) {
         req.logger.error(error);
