dd4fd5d3b1c2e51d1bea234b65dc479172b91671
Bruno Gastadi <gramado.f@gmail.com>
2021-07-25 20:55:14 -0300

    fix: chatwoot corrigido

--- src/controller/deviceController.js
+++ src/controller/deviceController.js
@@ -695,32 +695,23 @@ export async function starMessage(req, res) {
 }
 
 export async function chatWoot(req, res) {
-    console.log(req.body);
     const { session } = req.params;
     const client = clientsArray[session];
     try {
         if (await client.isConnected()) {
-            const event = req.body.event;
-  
-            if(event == 'conversation_status_changed'){
-                return res.status(200).json({ status: 'success', message: 'Success on receive chatwoot' });
-            }
-            
             const {
+                event,
                 message_type,
                 phone = req.body.conversation.meta.sender.phone_number,
                 message = req.body.conversation.messages[0],
             } = req.body;
-            
+
             if (event != 'message_created' && message_type != 'outgoing') return res.status(200);
+
             for (const contato of contactToArray(phone, false)) {
-                 if(message_type == 'outgoing'){ 
-                    if (message.attachments){
-                        await client.sendFile(`${contato}`, message.attachments[0].data_url, 'file', message.content);
-                    }else{
-                        await client.sendText(contato, message.content);                    
-                    }
-                }
+                if (message.attachments)
+                    await client.sendFile(`${contato}`, message.attachments[0].data_url, 'file', message.content);
+                else await client.sendText(contato, message.content);
             }
             return res.status(200).json({ status: 'success', message: 'Success on  receive chatwoot' });
         }
