a4797254687f9288f00be7724e1e4aca0435ab3d
Bruno Gastadi <gramado.f@gmail.com>
2021-09-06 11:53:58 -0300

    Revert "Revert "Criado logica para utilizar um prefix no banco Redis""

--- src/util/tokenStore/redisTokenStory.js
+++ src/util/tokenStore/redisTokenStory.js
@@ -1,10 +1,16 @@
 import redisClient from '../db/redis/db';
+import config from '../../config.json';
+import { getIPAddress } from '../functions';
 
 var RedisTokenStore = function (client) {
+  let prefix = config.db.redisPrefix || '';
+  if (prefix === 'docker') {
+    prefix = getIPAddress();
+  }
   this.tokenStore = {
     getToken: (sessionName) =>
       new Promise((resolve, reject) => {
-        redisClient.get(sessionName, (err, reply) => {
+        redisClient.get(prefix + sessionName, (err, reply) => {
           if (err) {
             return reject(err);
           }
@@ -15,22 +21,27 @@ var RedisTokenStore = function (client) {
       new Promise((resolve) => {
         tokenData.sessionName = sessionName;
         tokenData.config = client.config;
-        redisClient.set(sessionName, JSON.stringify(tokenData), (err) => {
+        redisClient.set(prefix + sessionName, JSON.stringify(tokenData), (err) => {
           return resolve(err ? false : true);
         });
       }),
     removeToken: (sessionName) =>
       new Promise((resolve) => {
-        redisClient.del(sessionName, (err) => {
+        redisClient.del(prefix + sessionName, (err) => {
           return resolve(err ? false : true);
         });
       }),
     listTokens: () =>
       new Promise((resolve) => {
-        redisClient.keys('*', (err, keys) => {
+        redisClient.keys(prefix + '*', (err, keys) => {
           if (err) {
             return resolve([]);
           }
+          keys.forEach((item, indice) => {
+            if (prefix !== '' && item.includes(prefix)) {
+              keys[indice] = item.substring(item.indexOf(prefix) + prefix.length);
+            }
+          });
           return resolve(keys);
         });
       }),
