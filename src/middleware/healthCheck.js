ecea4165637d8807471a5b17f23d29cab7839892
Bruno Gastadi <gramado.f@gmail.com>
2021-09-27 21:53:36 -0300

    feat: adicionado endpoint healthz

--- src/middleware/healthCheck.js
+++ src/middleware/healthCheck.js
@@ -15,17 +15,15 @@
  */
 
 export async function healthz(req, res) {
-    const healthcheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
-    try {
-        res.status(200).send(healthcheck);
-    } catch (e) {
-        healthcheck.message = e;
-        res.status(503).send();
-    }
-
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now()
+  };
+  try {
+    res.status(200).send(healthcheck);
+  } catch (e) {
+    healthcheck.message = e;
+    res.status(503).send();
+  }
 }
-
