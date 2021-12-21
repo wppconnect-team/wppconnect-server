c67d8c3faf4959ab5f31c15fb9ddad78e90a3640
GabrielBragaGit <65824599+GabrielBragaGit@users.noreply.github.com>
2021-12-21 10:26:16 -0300

    Evita criar conversas adicionais erroneamente
    O filtro de status estava sendo feito apenas para conversas com status 'open', porém existem os status 'pending' e 'snoozed' que também devem ser consideradas como conversas ainda abertas... principalmente quando um robô é utilizado para o atendimento, nesse caso todas as conversas iniciam no com status 'pending', antes de fazer esse ajuste no código não estava conseguindo dar seguimento no fluxo do meu dialogflow porque a cada nova resposta ele gerava uma nova conversa...

--- src/util/chatWootClient.js
+++ src/util/chatWootClient.js
@@ -171,9 +171,9 @@ export default class chatWootClient {
   async findConversation(contact) {
     try {
       const { data } = await this.api.get(
-        `api/v1/accounts/${this.account_id}/conversations?inbox_id=${this.inbox_id}&status=open`
+        `api/v1/accounts/${this.account_id}/conversations?inbox_id=${this.inbox_id}&status=all`
       );
-      return data.data.payload.find((e) => e.meta.sender.id == contact.id);
+      return data.data.payload.find((e) => e.meta.sender.id == contact.id && e.status != "resolved");
     } catch (e) {
       console.log(e);
       return null;
