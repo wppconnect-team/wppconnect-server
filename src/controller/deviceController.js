d53931c8fb21491b495f5463fe72d0edd7d66813
ghsgabriel <ghs.gabriel@outlook.com>
2022-01-17 10:25:28 -0300

    fix: Fix clearChat function

--- src/controller/deviceController.js
+++ src/controller/deviceController.js
@@ -235,18 +235,17 @@ export async function deleteChat(req, res) {
 }
 
 export async function clearChat(req, res) {
-  const { phone, isGroup = false } = req.body;
+  const { phone } = req.body;
+  const session = req.session;
 
   try {
-    if (isGroup) {
-      await req.client.clearChat(`${phone}@g.us`);
-    } else {
-      await req.client.clearChat(`${phone}@c.us`);
+    let results = {};
+    for (const contato of phone) {
+      results[contato] = await req.client.clearChat(contato);
     }
-    return res.status(200).json({ status: 'success', response: { message: 'Successfully cleared conversation' } });
-  } catch (e) {
-    req.logger.error(e);
-    return res.status(500).json({ status: 'error', message: 'Error clearing conversation' });
+    returnSucess(res, session, phone, results);
+  } catch (error) {
+    returnError(req, res, session, error);
   }
 }
 
