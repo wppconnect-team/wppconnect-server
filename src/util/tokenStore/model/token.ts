3da527254e0e302dcaafa4bb0d644e94ae203b58
Raimundo Pessoa <53197363+cybernetto@users.noreply.github.com>
2023-05-24 14:00:25 -0300

    Update token.ts
    Resolving some logic simple errors

--- src/util/tokenStore/model/token.ts
+++ src/util/tokenStore/model/token.ts
@@ -1,21 +1,14 @@
 import config from '../../../config';
+import mongoose from '../../db/mongodb/db';
 
-const mongoose =
-  config.tokenStoreType === 'mongodb' ? require('../../db/mongodb/db') : null;
-
-let Token: any = null;
-
-if (config.tokenStoreType === 'mongodb') {
-  const TokenSchema = new mongoose.Schema({
-    WABrowserId: String,
-    WASecretBundle: String,
-    WAToken1: String,
-    WAToken2: String,
-    webhook: String,
-    config: String,
-    sessionName: String,
-  });
-  Token = mongoose.model('Token', TokenSchema);
-}
+const Token = config.tokenStoreType === 'mongodb' ? mongoose.model('Token', new mongoose.Schema({
+  WABrowserId: String,
+  WASecretBundle: String,
+  WAToken1: String,
+  WAToken2: String,
+  webhook: String,
+  config: String,
+  sessionName: String,
+})) : null;
 
 export default Token;
