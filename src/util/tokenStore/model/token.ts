c04a0117c36d5daa7c57de587e0e4378631e6cc4
Raimundo Pessoa <53197363+cybernetto@users.noreply.github.com>
2023-06-02 00:04:30 -0300

    Formatting issues

--- src/util/tokenStore/model/token.ts
+++ src/util/tokenStore/model/token.ts
@@ -1,14 +1,20 @@
 import config from '../../../config';
 import mongoose from '../../db/mongodb/db';
 
-const Token = config.tokenStoreType === 'mongodb' ? mongoose.model('Token', new mongoose.Schema({
-  WABrowserId: String,
-  WASecretBundle: String,
-  WAToken1: String,
-  WAToken2: String,
-  webhook: String,
-  config: String,
-  sessionName: String,
-})) : null;
+const Token =
+  config.tokenStoreType === 'mongodb'
+    ? mongoose.model(
+        'Token',
+        new mongoose.Schema({
+          WABrowserId: String,
+          WASecretBundle: String,
+          WAToken1: String,
+          WAToken2: String,
+          webhook: String,
+          config: String,
+          sessionName: String,
+        })
+      )
+    : null;
 
 export default Token;
