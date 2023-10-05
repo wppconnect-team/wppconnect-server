377fc1343febc69da3d8fc12a2d5596fff5fe999
Fabio <57275833+fabioselau077@users.noreply.github.com>
2023-10-05 10:32:22 -0400

    Hotfix/ignore event from type webhook (#1515)
    * fix(config): ignore tag in webhook config
    
    * fix(functions): format return

--- src/types/ServerOptions.ts
+++ src/types/ServerOptions.ts
@@ -23,6 +23,7 @@ export interface ServerOptions {
     onPollResponse: boolean;
     onRevokedMessage: boolean;
     onSelfMessage: boolean;
+    ignore: string[];
   };
   archive: {
     enable: boolean;
