8eba71d4a19735730e435ca411da11c67cd349f9
Felipe Reinaux <felipereinaux@gmail.com>
2023-11-12 11:24:23 -0300

    ajustes

--- src/config.ts
+++ src/config.ts
@@ -38,7 +38,7 @@ export default {
     daysToArchive: 45,
   },
   log: {
-    level: 'debug', // Before open a issue, change level to silly and retry a action
+    level: 'silly', // Before open a issue, change level to silly and retry a action
     logger: ['console', 'file'],
   },
   createOptions: {
--- src/util/createSessionUtil.ts
+++ src/util/createSessionUtil.ts
@@ -58,6 +58,7 @@ export default class CreateSessionUtil {
       if (req.serverOptions.customUserDataDir) {
         req.serverOptions.createOptions.puppeteerOptions = {
           args: ['--no-sandbox'],
+          headless: true,
           executablePath:
             'node_modules\\chromium\\lib\\chromium\\chrome-win\\chrome.exe',
           userDataDir: req.serverOptions.customUserDataDir + session,
