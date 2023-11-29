371049540e6d7b84f5856c7deaf2c9883c26b053
Felipe Reinaux <felipereinaux@gmail.com>
2023-11-29 20:24:29 -0300

    ajustes

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,11 @@ export function initServer(serverOptions: any) {
   const app = express();
   const PORT = process.env.PORT || serverOptions.port;
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: '*',
+    })
+  );
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.use('/files', express.static('WhatsAppImages'));
