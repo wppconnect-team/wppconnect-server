654bdfb9e6e16fe3bec46c99ed46b5b122526ca1
Felipe Reinaux <felipereinaux@gmail.com>
2024-01-29 09:34:02 -0300

    ajuste versão

--- src/util/createSessionUtil.ts
+++ src/util/createSessionUtil.ts
@@ -157,7 +157,6 @@ export default class CreateSessionUtil {
       urlcode: urlCode,
     });
 
-    qrCode = qrCode.replace('data:image/png;base64,', '');
     const imageBuffer = Buffer.from(qrCode, 'base64');
 
     req.io.emit('qrCode', {
