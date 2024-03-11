b2e80cc519307ff3a7be98ed22076e480b5c816a
Felipe Reinaux <felipereinaux@gmail.com>
2024-03-11 10:43:33 -0300

    ajuste cepcontroller

--- src/controller/cepController.ts
+++ src/controller/cepController.ts
@@ -25,53 +25,53 @@ export async function getCep(req: Request, res: Response) {
       res.send(info);
     });
   } else {
-    axios
-      .get(
-        'https://www.cepaberto.com/api/v3/cep?cep=' +
-          req.params.cep.replace('-', ''),
-        {
-          timeout: 5000,
-          headers: {
-            Authorization: 'Token token=a30562962004d94271044de19730a8be',
-          },
-        }
-      )
-      .then((axiosReq) => {
-        const result = axiosReq.data;
+    // axios
+    //   .get(
+    //     'https://www.cepaberto.com/api/v3/cep?cep=' +
+    //       req.params.cep.replace('-', ''),
+    //     {
+    //       timeout: 5000,
+    //       headers: {
+    //         Authorization: 'Token token=a30562962004d94271044de19730a8be',
+    //       },
+    //     }
+    //   )
+    //   .then((axiosReq) => {
+    //     const result = axiosReq.data;
 
-        const info = {
-          cep: result.cep,
-          logradouro: result.logradouro,
-          bairro: result.bairro,
-          localidade: result.cidade.nome,
-          uf: result.estado.sigla,
-          ibge: result.cidade.ibge,
-          ddd: result.cidade.ddd,
-          lat: result.latitude,
-          lon: result.longitude,
-        };
+    //     const info = {
+    //       cep: result.cep,
+    //       logradouro: result.logradouro,
+    //       bairro: result.bairro,
+    //       localidade: result.cidade.nome,
+    //       uf: result.estado.sigla,
+    //       ibge: result.cidade.ibge,
+    //       ddd: result.cidade.ddd,
+    //       lat: result.latitude,
+    //       lon: result.longitude,
+    //     };
 
-        const data = JSON.stringify(info);
-        fs.writeFileSync(req.params.cep.replace('-', '') + '.json', data);
-        res.send(info);
-      })
-      .catch(() => {
-        correios.consultaCEP({ cep: req.params.cep }).then((resultCorreio) => {
-          const info = {
-            cep: resultCorreio.cep,
-            logradouro: resultCorreio.address,
-            bairro: resultCorreio.district,
-            localidade: resultCorreio.city,
-            uf: resultCorreio.state,
-            ibge: resultCorreio.city_ibge,
-            ddd: resultCorreio.ddd,
-            lat: resultCorreio.lat,
-            lon: resultCorreio.lng,
-          };
-          const data = JSON.stringify(info);
-          fs.writeFileSync(req.params.cep.replace('-', '') + '.json', data);
-          res.send(info);
-        });
-      });
+    //     const data = JSON.stringify(info);
+    //     fs.writeFileSync(req.params.cep.replace('-', '') + '.json', data);
+    //     res.send(info);
+    //   })
+    //   .catch(() => {
+    correios.consultaCEP({ cep: req.params.cep }).then((resultCorreio) => {
+      const info = {
+        cep: resultCorreio.cep,
+        logradouro: resultCorreio.address,
+        bairro: resultCorreio.district,
+        localidade: resultCorreio.city,
+        uf: resultCorreio.state,
+        ibge: resultCorreio.city_ibge,
+        ddd: resultCorreio.ddd,
+        lat: resultCorreio.lat,
+        lon: resultCorreio.lng,
+      };
+      const data = JSON.stringify(info);
+      fs.writeFileSync(req.params.cep.replace('-', '') + '.json', data);
+      res.send(info);
+    });
+    // });
   }
 }
