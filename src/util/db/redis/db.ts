1a56c6f448c1a2a58a0abab9460ab103acc325b6
Felipe Reinaux <felipereinaux@gmail.com>
2024-07-11 12:02:44 -0300

    redis url

--- src/util/db/redis/db.ts
+++ src/util/db/redis/db.ts
@@ -1,3 +1,5 @@
+import { url } from 'inspector';
+
 import config from '../../../config';
 
 const redis = config.tokenStoreType === 'redis' ? require('redis') : null;
@@ -5,9 +7,8 @@ const redis = config.tokenStoreType === 'redis' ? require('redis') : null;
 let RedisClient: any = null;
 
 if (config.tokenStoreType === 'redis') {
-  RedisClient = redis.createClient(config.db.redisPort, config.db.redisHost, {
-    password: config.db.redisPassword,
-    db: config.db.redisDb,
+  RedisClient = redis.createClient({
+    url: 'redis://:O83feb7drQ8u4zJcqLSEKmePDhknFGN9GeQ0UTio4hVhW1GZ8SNq2FkCNrK10QBb@bk8444k:6379/0',
   });
 }
 
