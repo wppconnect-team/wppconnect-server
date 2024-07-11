272bfbb2d66d127a0d529276d58ab417f89a3584
Felipe Reinaux <felipereinaux@gmail.com>
2024-07-11 12:09:52 -0300

    ajuste

--- src/util/db/redis/db.ts
+++ src/util/db/redis/db.ts
@@ -7,9 +7,12 @@ const redis = config.tokenStoreType === 'redis' ? require('redis') : null;
 let RedisClient: any = null;
 
 if (config.tokenStoreType === 'redis') {
-  RedisClient = redis.createClient({
-    url: 'redis://:O83feb7drQ8u4zJcqLSEKmePDhknFGN9GeQ0UTio4hVhW1GZ8SNq2FkCNrK10QBb@bk8444k:6379/0',
-  });
+  RedisClient = redis
+    .createClient({
+      url: 'redis://:O83feb7drQ8u4zJcqLSEKmePDhknFGN9GeQ0UTio4hVhW1GZ8SNq2FkCNrK10QBb@bk8444k:6379/0',
+    })
+    .on('error', (err) => console.error('Redis Client Error', err))
+    .connect();
 }
 
 export default RedisClient;
