95aed0988e96800b82af3de7329c68c4a9627777
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2025-07-31 19:10:57 -0300

    fix: ajuste de contato para validar internacional e brasileiro

--- src/util/chatWootClient.ts
+++ src/util/chatWootClient.ts
@@ -249,15 +249,15 @@ export default class chatWootClient {
     }
   }
   normalizeBrazillianNumber(number: string): string {
-    if (number.startsWith('55')) {
-      number = number.slice(2);
-    } else if (number.length > 11 || number.startsWith('0')) {
-      number = number.startsWith('+') ? number.slice(3) : number.slice(1);
+    number = number.replace('+', '');
+    if (!number.startsWith('55')) {
+      return number;
     }
+    number = number.slice(2);
     if (number.length === 10 && ['8', '9'].includes(number[2])) {
       number = number.slice(0, 2) + '9' + number.slice(2);
     }
-    return '+55' + number;
+    return '55' + number;
   }
 
   async findContact(query: string) {
@@ -286,7 +286,7 @@ export default class chatWootClient {
           : message.sender.id.split('@')[0],
     };
     body.phone_number = this.normalizeBrazillianNumber(body.phone_number);
-    const contact = await this.findContact(body.phone_number.replace('+', ''));
+    const contact = await this.findContact(body.phone_number);
     if (contact && contact.meta.count > 0) return contact.payload[0];
 
     try {
