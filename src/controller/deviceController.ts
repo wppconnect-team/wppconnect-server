eb91101a9900fe0cea639efcaa3be452668a4344
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2025-11-25 18:48:51 -0300

    fix: typos and started using StatusFind enum

--- src/controller/deviceController.ts
+++ src/controller/deviceController.ts
@@ -1818,7 +1818,7 @@ export async function setRecording(req: Request, res: Response) {
     let response;
     for (const contato of contactToArray(phone, isGroup)) {
       if (value) response = await req.client.startRecording(contato, duration);
-      else response = await req.client.stopRecoring(contato);
+      else response = await req.client.stopRecording(contato);
     }
 
     res.status(200).json({ status: 'success', response: response });
