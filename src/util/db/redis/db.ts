28a6dbcdc5df00a5e7aa753c42f6583f537ab1c5
alphabet90 <93392677+alphabet90@users.noreply.github.com>
2025-11-28 10:38:53 -0300

    Fix connection with redis

--- src/util/db/redis/db.ts
+++ src/util/db/redis/db.ts
@@ -1,13 +1,32 @@
 import config from '../../../config';
+import { createLogger } from '../../util/logger';
+
+const logger = createLogger(config.log);
 
 const redis = config.tokenStoreType === 'redis' ? require('redis') : null;
 
-let RedisClient: any = null;
+let RedisClient = null;
 
 if (config.tokenStoreType === 'redis') {
-  RedisClient = redis.createClient(config.db.redisPort, config.db.redisHost, {
+  RedisClient = redis.createClient({
+    socket: {
+      host: config.db.redisHost,
+      port: config.db.redisPort
+    },
     password: config.db.redisPassword,
-    db: config.db.redisDb,
+    database: config.db.redisDb
+  });
+
+  RedisClient.connect().catch((err) => {
+    logger.error('Error connecting to Redis:', err);
+  });
+  
+  RedisClient.on('error', (err) => {
+    logger.error('Redis Client Error', err);
+  });
+  
+  RedisClient.on('connect', () => {
+    logger.info('Connected to Redis successfully');
   });
 }
 
