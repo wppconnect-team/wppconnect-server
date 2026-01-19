8bee7a19bc90d85f5c5128a78ef75713eae3bcd8
Eduardo Balbino <eduardobalbino1337@gmail.com>
2026-01-19 16:13:32 -0300

    removing ternary for better readability

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -227,9 +227,11 @@ export async function autoDownload(client: any, req: any, message: any) {
           })
         );
 
-        message.fileUrl = config?.aws_s3?.endpoint
-          ? `${config.aws_s3.endpoint}/${bucketName}/${fileName}`
-          : `https://${bucketName}.s3.amazonaws.com/${fileName}`;
+        if (config?.aws_s3?.endpoint) {
+          message.fileUrl = `${config.aws_s3.endpoint}/${bucketName}/${fileName}`;
+        } else {
+          message.fileUrl = `https://${bucketName}.s3.amazonaws.com/${fileName}`;
+        }
       } else {
         message.body = await buffer.toString('base64');
       }
